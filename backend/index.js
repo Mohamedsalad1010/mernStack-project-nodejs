5e4111ebcc86b8903e219d32bcace2e75768660a
mohamed salad <abdikariimsalad55@gmail.com>
2026-02-21 22:47:37 +0300

    add online link

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(limiter);
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173"]
+        origin: ["http://localhost:5173" , 'https://mernstack-project-nodejs.onrender.com']
     }
 ))
 if (process.env.NODE_ENV === "development") {
